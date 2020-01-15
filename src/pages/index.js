3d27fcd5aa155624672e4ee674589b5c9d67abe2
john-src <holman.jx@gmail.com>
2020-01-15 10:13:55 -0500

    add jest, config, + quick snapshot test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-// just a simple POC, will remove later
 import ProduceGrid from "../components/produce/produceGrid" 
 
 import SEO from "../components/seo"
@@ -10,10 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h2>The best available produce.</h2>
+    <h2>The best available produce</h2>
     <h4>January</h4>
     <ProduceGrid />
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
