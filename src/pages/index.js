7b46bab60f1878f7e2e7e7f18db359d127d013b0
john-src <holman.jx@gmail.com>
2020-01-14 21:04:55 -0500

    code organization, light UI work

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 // just a simple POC, will remove later
-import Payload from "../components/payload" 
+import ProduceGrid from "../components/produce/produceGrid" 
+
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Payload />
+    <h2>The best available produce.</h2>
+    <h4>January</h4>
+    <ProduceGrid />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
